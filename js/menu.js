3e0caaeb3dc4dfc5068b85d240ce9b7a3689086d
Lilian Carvalho Santos Ribeiro <kleidk74@gmail.com>
2023-04-25 15:30:59 -0300

    Ajustando o menu responsivo

--- js/menu.js
+++ js/menu.js
@@ -5,4 +5,12 @@ const icone = document.querySelector(".icone");
 botao.addEventListener("click", function(event){
     event.preventDefault();
     linksMenu.classList.toggle("aberto");
+
+    /* Se a classe "aberto" estiver aplicada ao "links menu", mude o texto/icone para "fechar X". Caso contrário, mostre o texto/icone para "Menu &equiv;" */
+
+    if(linksMenu.classList.contains("aberto")){
+        icone.innerHTML = "Fechar &times;";
+    } else {
+        icone.innerHTML = "Menu &equiv;"; 
+    }
 });
